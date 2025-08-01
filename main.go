14bb4ab8300ce980c16efcf550375aef63b67ef3
Jason Leitch <jason.leitch@capgemini.com>
2025-08-01 23:54:17 +0100

    backend

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Job struct {
+	ID string `json:"id"`
+}
+
+var jobs = []Job{}
+
+/*
+create jobs
+get jobs
+get job
+
+set header with bad ip and path
+use apim c# policy to rewrite header to client
+use apim c# policy to forward get operation back
+*/
+
+func uuid() int {
+	return rand.Int()
+}
+
+func getJobs(c *gin.Context) {
+	c.JSON(http.StatusOK, jobs)
+}
+
+func getJob(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, job := range jobs {
+		if job.ID == id {
+			c.JSON(http.StatusOK, job)
+			return
+		}
+	}
+}
+
+func createJob(c *gin.Context) {
+	var job Job
+	uid := strconv.Itoa(uuid())
+	jobs = append(jobs, Job{ID: uid})
+	c.Header("operation-location", "http://10.10.10.10/my/long/path/job/"+uid)
+	c.JSON(http.StatusCreated, job)
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/job/create", createJob)
+	router.GET("/job/:id", getJob)
+	router.GET("/jobs", getJobs)
+
+	router.Run("localhost:8080")
+}
